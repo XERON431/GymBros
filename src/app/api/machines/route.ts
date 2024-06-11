be9472a99721905c2d80630f7b03c0103e88da07
Siddhant Goyal <g.siddhant@iitg.ac.in>
2024-06-11 12:01:09 +0530

    fixed type errors

--- src/app/api/machines/route.ts
+++ src/app/api/machines/route.ts
@@ -1,5 +1,5 @@
-// app/api/machines/route.js
 import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
 import connectDB from '../../../utils/db';
 import Machine from '../../../models/Machines'; // Make sure to import your Machine model
 
@@ -9,11 +9,12 @@ export async function GET() {
     const machines = await Machine.find({});
     return NextResponse.json({ success: true, data: machines });
   } catch (error) {
-    return NextResponse.json({ success: false, error: error.message });
+    const err = error as Error;
+    return NextResponse.json({ success: false, error: err.message });
   }
 }
 
-export async function POST(req) {
+export async function POST(req: NextRequest) {
   await connectDB();
   try {
     const body = await req.json();
@@ -21,6 +22,7 @@ export async function POST(req) {
     await machine.save();
     return NextResponse.json({ success: true, data: machine });
   } catch (error) {
-    return NextResponse.json({ success: false, error: error.message });
+    const err = error as Error;
+    return NextResponse.json({ success: false, error: err.message });
   }
 }
