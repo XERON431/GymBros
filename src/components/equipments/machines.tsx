53f34cf7d9556f0101c04a33cf548eaf3f7ce730
Siddhant Goyal <g.siddhant@iitg.ac.in>
2024-06-12 10:28:12 +0530

    loader added in exercise page

--- src/components/equipments/machines.tsx
+++ src/components/equipments/machines.tsx
@@ -20,37 +20,36 @@ const StrengthTrainingMachines: React.FC = () => {
   const [selectedExercise, setSelectedExercise] = useState<Exercise | null>(null);
   const [categories, setCategories] = useState<Category[]>([]);
   const [expandedCategory, setExpandedCategory] = useState<string | null>(null);
-  const [loading, setLoading] = useState(true);  // State to track loading
+  const [isLoading, setIsLoading] = useState(true); // Flag for loading state
 
   useEffect(() => {
+    const fetchExercises = async () => {
+      try {
+        const response = await fetch('/api/exercises');
+        const result = await response.json();
+        if (result.success) {
+          const fetchedCategories: Category[] = result.data.reduce((acc: Category[], exercise: Exercise) => {
+            const categoryIndex = acc.findIndex((cat) => cat.name === exercise.category);
+            if (categoryIndex !== -1) {
+              acc[categoryIndex].exercises.push(exercise);
+            } else {
+              acc.push({ name: exercise.category, exercises: [exercise] });
+            }
+            return acc;
+          }, []);
+          setCategories(fetchedCategories);
+        } else {
+          console.error(result.error);
+        }
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setIsLoading(false); // Always set loading to false after fetching
+      }
+    };
+
     fetchExercises();
   }, []);
-
-  const fetchExercises = async () => {
-    setLoading(true);  // Start loading
-    try {
-      const response = await fetch('/api/exercises');
-      const result = await response.json();
-      if (result.success) {
-        const fetchedCategories: Category[] = result.data.reduce((acc: Category[], exercise: Exercise) => {
-          const categoryIndex = acc.findIndex((cat) => cat.name === exercise.category);
-          if (categoryIndex !== -1) {
-            acc[categoryIndex].exercises.push(exercise);
-          } else {
-            acc.push({ name: exercise.category, exercises: [exercise] });
-          }
-          return acc;
-        }, []);
-        setCategories(fetchedCategories);
-      } else {
-        console.error(result.error);
-      }
-    } catch (error) {
-      console.error(error);
-    } finally {
-      setLoading(false);  // Stop loading
-    }
-  };
 
   const handleExerciseModalClose = () => {
     setExerciseModalOpen(false);
@@ -83,55 +82,53 @@ const StrengthTrainingMachines: React.FC = () => {
   return (
     <div>
       <AddExerciseForm onExerciseAdded={handleExerciseAdded} />
-      {loading ? (
-        <Box display="flex" justifyContent="center" alignItems="center" minHeight="100vh">
-          <CircularProgress />
-        </Box>
+      {isLoading ? (
+        <div style={{ textAlign: 'center', padding: 20 }}>
+          <CircularProgress color="secondary" />
+          <Typography variant="body2" gutterBottom>Loading exercises...</Typography>
+        </div>
       ) : (
-        categories.map((category, index) => (
-          <Box key={index}>
-            <Typography
-              variant="h6"
-              color={expandedCategory === category.name ? "primary" : "initial"}
-              onClick={() => toggleExpansion(category.name)}
-              style={{ cursor: "pointer", fontWeight: "bold" }}
-            >
-              {category.name}
-            </Typography>
-            {expandedCategory === category.name && (
-              <List>
-                {category.exercises.map((exercise, index) => (
-                  <ListItem
-                    key={index}
-                    sx={{
-                      '&:hover': {
-                        backgroundColor: '#f0f0f0',
-                      },
-                    }}
-                  >
-                    <ListItemText
-                      primary={`${exercise.name} - Sets: ${exercise.sets}, Reps: ${exercise.reps}`}
-                      primaryTypographyProps={{ color: "black" }}
-                      onClick={() => handleExerciseClick(exercise)}
-                    />
-                  </ListItem>
-                ))}
-              </List>
-            )}
-          </Box>
-        ))
+        <>
+          
+          {categories.map((category, index) => (
+            <Box key={index}>
+              <Typography
+                variant="h6"
+                color={expandedCategory === category.name ? "primary" : "initial"}
+                onClick={() => toggleExpansion(category.name)}
+                style={{ cursor: "pointer", fontWeight: "bold" }}
+              >
+                {category.name}
+              </Typography>
+              {expandedCategory === category.name && (
+                <List>
+                  {category.exercises.map((exercise, index) => (
+                    <ListItem key={index} sx={{ '&:hover': { backgroundColor: '#f0f0f0' } }}>
+                      <ListItemText
+                        primary={`${exercise.name} - Sets: ${exercise.sets}, Reps: ${exercise.reps}`}
+                        primaryTypographyProps={{ color: "black" }}
+                        onClick={() => handleExerciseClick(exercise)}
+                      />
+                    </ListItem>
+                  ))}
+                </List>
+              )}
+            </Box>
+          ))}
+        </>
       )}
-      <CustomModal
+       <CustomModal
         open={exerciseModalOpen}
         handleClose={handleExerciseModalClose}
         title={selectedExercise ? selectedExercise.name : ""}
-        fullscreen={false}
+        fullscreen={false} // or true, depending on your needs
       >
         {selectedExercise && (
           <Box>
             <Typography variant="body1">
               Exercise Details: Sets - {selectedExercise.sets}, Reps - {selectedExercise.reps}
             </Typography>
+            {/* You can add more details or components here based on your requirements */}
           </Box>
         )}
       </CustomModal>
